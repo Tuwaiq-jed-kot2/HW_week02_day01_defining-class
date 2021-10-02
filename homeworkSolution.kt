bc9bab3166c741088da81f82eb6ea929982ef532
Anas Alqassim <77699046+anasalqassim@users.noreply.github.com>
2021-10-02 20:33:15 +0300

    good

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,6 +1,29 @@
-# Kotlin Programming 
----
-## Learning Objectives ✨
-- defining class
-## Getting started 
-Open the [assignment.md](assignment.md) file to get started.
+Answer Q1
+
+fun main() {
+
+
+    println(" name student ")
+    var name: String? = readLine()!!
+
+    println(" age student ")
+    var age: Int = readLine()!!.toInt()
+
+    println("GPA student ")
+    var GPA: Double = readLine()!!.toDouble()
+
+
+    class Student(val Name: String, val Age: Int, val GPA: Double) {
+
+
+    }
+}
+///Answer Q2
+Race conditions occur when two threads interact in
+a negatve way depending on the exact order
+that their different instruction are executed
+Answer Q3///
+Yes, In the <<println(weapon.name)
+Answer Q4//
+we must use a scope function, maybe in this situation use also
+
