63d5a38cdf0fe5553b8e0fd91d91d3634dcaf5c8
AbdulghaniAlhazmi <Abdulghani.Hassan.jed.k2@tuwaiq.edu.sa>
2021-10-17 20:56:04 +0300

    solved

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -4,3 +4,29 @@
 - defining class
 ## Getting started 
 Open the [assignment.md](assignment.md) file to get started.
+
+//Part 1
+
+fun main() {
+    var x = Students("Abdulghani", 24, 3.40)
+    x.studentInfo()
+    x.speak()
+    x.add(7, 5)
+
+}
+
+class Students(var name: String, var age: Int, var gpa: Double) {
+    fun studentInfo() {
+        println("Nmae: $name \nAge: $age \nGPA: $gpa")
+    }
+
+    fun speak() {
+        println("Student can speak")
+    }
+
+    fun add(num1: Int, num2: Int) {
+        val result: Int = num1 + num2
+        println("$num1 + $num2 = $result")
+    }
+
+}
