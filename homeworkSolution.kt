a4ae63f50a54994e06efbf25d1c9757749f2d23f
shahad197 <shahad.yousef.jed.k2@tuwaiq.edu.sa>
2021-10-17 21:39:32 +0300

    donee

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,6 +1,33 @@
-# Kotlin Programming 
----
-## Learning Objectives ✨
-- defining class
-## Getting started 
-Open the [assignment.md](assignment.md) file to get started.
+
+
+fun main(){
+
+    class univerSity(name:String,Age:Int,Gpa:Double)
+    val studentName: String
+    val studentId: Int
+    val studentGpa: Double
+
+    fun speake (name:String,Age:Int,Gpa:Double){
+        println("hi.my name is $name,my $Age,my $Gpa")
+
+    }
+    fun add (n1:Int,n2:Int){
+        println(n1 + n2)
+    }
+
+}
+/*
+Q1 = what dose Race Conditions mean
+Race conditions occur when two threads interact in a negatve way depending on the exact order that their different instruction are executed
+ */
+
+/*
+Q2 = is there any Race Conditions in the code
+yes
+ */
+
+/*
+Q3 = what's the best way to solve the compiling error in the code using scopeFunction
+
+ by also scope function
+ */
